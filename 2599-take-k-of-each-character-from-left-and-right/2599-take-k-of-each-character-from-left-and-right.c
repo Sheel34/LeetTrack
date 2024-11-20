5da601f2306b03277c95a5e87e1d16485af21349
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2024-11-20 22:30:55 +0530

    Update 2599-take-k-of-each-character-from-left-and-right.c

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.c
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.c
@@ -1,11 +1,11 @@
-int takeCharacters(char* s, int k) {
-    int n = strlen(s);
+int takeCharacters (char* s, int k) {
+    int n = strlen (s);
     int a = 0, b = 0, c = 0;
     for (int i = 0; i < n; i++) 
     {
-        if (s[i] == 'a') a++;
-        else if (s[i] == 'b') b++;
-        else if (s[i] == 'c') c++;
+        if (s [i] == 'a') a++;
+        else if (s [i] == 'b') b++;
+        else if (s [i] == 'c') c++;
     }
     a -= k;
     b -= k;
@@ -15,14 +15,14 @@ int takeCharacters(char* s, int k) {
     int i = 0, j = 0, max_len = 0;
     while (j < n) 
     {
-        if (s[j] == 'a') a--;
-        else if (s[j] == 'b') b--;
-        else if (s[j] == 'c') c--;
+        if (s [j] == 'a') a--;
+        else if (s [j] == 'b') b--;
+        else if (s [j] == 'c') c--;
         while (a < 0 || b < 0 || c < 0) 
         {
-            if (s[i] == 'a') a++;
-            else if (s[i] == 'b') b++;
-            else if (s[i] == 'c') c++;
+            if (s [i] == 'a') a++;
+            else if (s [i] == 'b') b++;
+            else if (s [i] == 'c') c++;
             i++;
         }
         max_len = (j - i + 1 > max_len) ? j - i + 1 : max_len;
