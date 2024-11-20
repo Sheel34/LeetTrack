41821f7dac0968ef2bef6a30f39a6a890d80cbf8
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2024-11-20 22:26:19 +0530

    Time: 119 ms (98.89%), Space: 17 MB (95.9%) - LeetHub

---
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,22 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        freq = [0] * 3
+        size = len (s)
+        
+        for char in s:
+            freq [ord (char) - ord ('a')] += 1
+        
+        left = 0
+        right = 0
+        
+        if freq [0] < k or freq [1] < k or freq [2] < k:
+            return -1
+        
+        for right in range (size):
+            freq [ord (s [right]) - ord ('a')] -= 1
+            
+            if freq [0] < k or freq [1] < k or freq [2] < k:
+                freq [ord (s [left]) - ord ('a')] += 1
+                left += 1
+        
+        return size - (right - left + 1)
