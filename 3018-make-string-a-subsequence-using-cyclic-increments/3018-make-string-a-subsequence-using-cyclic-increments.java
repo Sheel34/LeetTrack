b16779722408612fe5f45195f7857f713caa0ab3
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2024-12-04 19:44:00 +0530

    Time: 5 ms (100%), Space: 45.4 MB (68.42%) - LeetHub

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        
+        int targetIdx = 0, targetLen = str2.length ();
+        
+        for (char currChar : str1.toCharArray ()) {
+            if (targetIdx < targetLen && (str2.charAt (targetIdx) - currChar + 26) % 26 <= 1) {
+                targetIdx++;
+            }
+        }
+        return targetIdx == targetLen;
+    }
+}
