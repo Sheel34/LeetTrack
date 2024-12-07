ce3acc4f1e16b0e9969b5a343b305537e93d7cec
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2024-12-07 12:36:29 +0530

    Time: 34 ms (50%), Space: 16.2 MB (33.33%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.c
@@ -0,0 +1,34 @@
+int canDivide (int* nums, int numsSize, int maxOperations, int penalty) {
+    int operations = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums [i] > penalty) {
+            operations += (nums [i] - 1) / penalty;
+            if (operations > maxOperations) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int minimumSize (int* nums, int numsSize, int maxOperations) {
+
+    int left = 1, right = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums [i] > right) {
+            right = nums [i];
+        }
+    }
+
+    int result = right;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (canDivide (nums, numsSize, maxOperations, mid)) {
+            result = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return result;
+}
