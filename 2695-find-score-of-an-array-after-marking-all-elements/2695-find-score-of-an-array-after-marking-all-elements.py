64e56c63be0d08dfc1d416ae9fb7ff4e517e64a1
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2024-12-13 08:16:14 +0530

    Time: 43 ms (99.27%), Space: 28.9 MB (98.71%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findScore (self, nums : List [int]) -> int:
+        score = 0
+        n = len (nums)
+        q = deque ()
+
+        for i in range (n):
+            if q and nums [i] >= q [-1]:
+                skip = False
+                while q:
+                    add = q.pop ()
+                    if not skip:
+                        score += add
+                    skip = not skip
+                continue
+            q.append (nums [i])
+        skip = False
+        while q:
+            add = q.pop ()
+            if not skip:
+                score += add
+            skip = not skip
+
+        return score
