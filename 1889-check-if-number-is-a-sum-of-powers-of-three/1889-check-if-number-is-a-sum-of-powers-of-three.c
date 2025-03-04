39232ac1cbc052cbeaa6266aa87c5ae99247c173
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-03-04 17:44:10 +0530

    Time: 0 ms (100%), Space: 8 MB (16.22%) - LeetHub

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.c
@@ -0,0 +1,9 @@
+bool checkPowersOfThree (int n) {
+    while (n > 0) {
+        int remainder = n % 3;
+        if (remainder == 2)
+            return false;
+        n = n / 3;
+    }
+    return true;  
+}
