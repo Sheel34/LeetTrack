7686432e7520ccd05c7eccb193c33f07ce54a702
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-04-08 11:34:47 +0530

    Time: 11 ms (9.86%), Space: 17.8 MB (73.67%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        ans = 0
+        while True:
+            count = defaultdict (int)
+            isDistinct = True
+            for n in nums [ans * 3 :]:
+                count [n] += 1
+                if count [n] > 1:
+                    isDistinct = False
+                    break
+            if isDistinct:
+                return ans
+            ans += 1
