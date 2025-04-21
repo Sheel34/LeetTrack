f9dcc36ea066c28ca264e98cd505fbf23cf2adc9
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-04-21 09:14:30 +0530

    Time: 157 ms (38.38%), Space: 32.4 MB (57.58%) - LeetHub

---
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        a = maxx = minn = 0
+        for i in differences:
+            a += i
+            maxx = max (maxx, a)
+            minn = min (minn, a)
+        return max (0, (upper - lower) - (maxx - minn) + 1)
