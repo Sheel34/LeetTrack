8e2fa6583a1d0623a220adc07215470fb5b2a8fd
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-05-13 08:26:16 +0530

    Time: 1358 ms (0%), Space: 11.5 MB (66.67%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.c
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.c
@@ -2,13 +2,13 @@ int lengthAfterTransformations (char* s, int t) {
     int MOD = 1000000007;
         long long freq [26] = {};
         for (int i = 0; i < strlen (s); i++)
-            freq[s [i] - 'a']++;
-        while (t >= 26){
-            long long z = freq[25];
+            freq [s [i] - 'a']++;
+        while (t >= 26) {
+            long long z = freq [25];
             for (int i = 24; i >= 0; i--)
-                freq[i + 1] = (freq[i + 1] + freq[i]) % MOD;
-            freq[0] += z;
-            freq[1] += z;
+                freq [i + 1] = (freq [i + 1] + freq [i]) % MOD;
+            freq [0] += z;
+            freq [1] += z;
             t -= 26;
         }
         int res = 0;
