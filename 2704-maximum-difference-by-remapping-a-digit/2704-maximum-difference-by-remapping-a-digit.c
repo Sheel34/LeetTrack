c10db9715863b96602f3b0c95ba490ab408404cd
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-06-14 07:46:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.95%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.c
@@ -0,0 +1,15 @@
+int minMaxDifference(int num) {
+    int min_v = num, max_v = num;
+    for (int i = 0; i < 10; ++i) {
+        int lo = 0, hi = 0, mul = 1;
+        for (int n = num; n; n /= 10) {
+            bool replace = n % 10 == i;
+            lo += (replace ? 0 : n % 10) * mul;
+            hi += (replace ? 9 : n % 10) * mul;
+            mul *= 10;
+        }
+        min_v = fmin(min_v, lo);
+        max_v = fmax(max_v, hi);
+    }
+    return max_v - min_v;
+}
