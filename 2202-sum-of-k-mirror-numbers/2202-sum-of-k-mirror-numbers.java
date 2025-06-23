c9f72db65433ccafb7225bf1e53a7a86f8688c6a
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-06-23 12:56:14 +0530

    Time: 200 ms (85.71%), Space: 40.8 MB (75%) - LeetHub

---
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long kMirror (int k, int n) {
+        long ans = 0;
+        int cnt = 0;
+        long [] pow10 = new long [12];
+        pow10 [0] = 1;
+        for (int i = 1; i < 12; i++) pow10 [i] = pow10 [i - 1] * 10;
+        while (cnt < n){
+            int len = 1;
+            for (; ; len++) {
+                int half = (len + 1) / 2;
+                long start = pow10 [half - 1], end = pow10 [half];
+                for (long h = (half == 1 ? 1 : start); h < end && cnt < n; h++) {
+                    long t = h, rev = 0, x = h;
+                    if((len & 1) == 1) t /= 10;
+                    while (t > 0) {
+                        rev = rev * 10 + t % 10;
+                        t /= 10;
+                    }
+                    long p = x * pow10 [len - half] + rev;
+                    if (isPal (p,k)){
+                        ans += p;
+                        cnt++;
+                        if (cnt == n) return ans;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    private boolean isPal (long x,int k) {
+        long t = x, rev = 0;
+        while (t > 0) {
+            rev = rev * k + t % k;
+            t /= k;
+        }
+        return rev==x;
+    }
+}
