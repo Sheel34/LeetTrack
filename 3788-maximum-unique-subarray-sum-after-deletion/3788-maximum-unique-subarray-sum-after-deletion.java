6233ef240bd68a4c9c660fe64aca8ab7e37c4f49
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-07-25 22:01:33 +0530

    Time: 1 ms (100%), Space: 42.7 MB (39.38%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 0; i < nums.length; ++i) {
             freq [nums [i] + 100]++;
             if (nums [i] > maxNum) maxNum = nums [i];
-        }
-        for (int i = 0; i < 201; ++i){
+        } 
+        for (int i = 0; i < 201; ++i) {
             int num = i - 100;
             if (freq [i] > 0 && num > 0)
                 result += num;
