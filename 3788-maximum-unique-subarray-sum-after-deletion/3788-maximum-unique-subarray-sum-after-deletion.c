bd03ef42bf6b610feec3130a77302cbdd18b5a59
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-07-25 22:01:42 +0530

    Time: 1 ms (12.5%), Space: 10.1 MB (6.25%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.c
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.c
@@ -1,11 +1,11 @@
 int maxSum (int* nums, int numsSize) {
     int freq [201] = {0};    
     int result = 0, max_num = INT_MIN;
-    for (int i = 0; i < numsSize; ++i){
+    for (int i = 0; i < numsSize; ++i) {
         freq [nums [i] + 100]++;
         if (nums [i] > max_num) max_num = nums [i];
     }
-    for (int i = 0; i < 201; ++i){
+    for (int i = 0; i < 201; ++i) {
         int num = i - 100;
         if (freq [i] > 0 && num > 0)
             result += num;
