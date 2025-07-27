af0c4c4e95e59e604cb8db2d6a66710b366c9800
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-07-27 11:52:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.19%) - LeetHub

---
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.c
@@ -0,0 +1,12 @@
+int countHillValley (int* nums, int numsSize) {
+    int count  =0;
+    bool diff [2] = {0, 0};
+    for (int i = 1; i < numsSize; i++){
+        if (nums [i - 1] == nums [i]) continue;
+        bool bigger = nums [i] > nums [i - 1];
+        diff [bigger] = 1;
+        count += diff [bigger] && diff [!bigger];
+        diff [!bigger] = 0;
+    }
+    return count;
+}
