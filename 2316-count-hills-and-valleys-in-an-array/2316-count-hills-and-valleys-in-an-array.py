37377f67b0bbc84835e298af75ed332e573f9af6
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-07-27 12:02:44 +0530

    Time: 1 ms (29.64%), Space: 17.7 MB (90.84%) - LeetHub

---
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countHillValley (self, nums : List [int]) -> int:
+        n, prev, count = len (nums), nums [0], 0
+        diff = [0, 0]; i = 0
+        while i < n:
+            while i < n and prev == nums [i]: i += 1
+            if i == n: break
+            bigger = 1 if nums [i] > prev else 0
+            diff [bigger] = 1
+            count += diff [bigger] and diff [1 - bigger]
+            diff [1 - bigger] = 0
+            prev = nums[i]
+            i += 1
+        return count
