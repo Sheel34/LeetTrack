53f63b6d6699511fb1dacd1e15655d6f8e21e096
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-08-05 09:47:18 +0530

    Time: 4 ms (15.79%), Space: 9.9 MB (57.89%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.c
@@ -0,0 +1,16 @@
+int numOfUnplacedFruits (int* fruits, int fruitsSize, int* baskets, int basketsSize) {
+    int unplaced = 0;
+    for (int i = 0; i < fruitsSize; i++) {
+        int placed = 0;
+        for (int j = 0; j < basketsSize; j++) {
+            if (fruits [i] <= baskets [j]) {
+                baskets [j] = 0; 
+                placed = 1;
+                break; 
+            }
+        }
+        if (!placed)
+            unplaced++;
+    }
+    return unplaced;
+}
