d5276904a31b246fc4de0266e1f9aa029f0b54f1
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-08-10 12:24:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (66.67%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.c
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.c
@@ -1,14 +1,14 @@
 bool check (int n, int freq []) {
-        int count [10] = {};
-        while (n > 0){
-            count [n % 10]++;
-            n /= 10;
-        }
-        for (int i = 0; i < 10; i++)
-            if (freq [i] != count [i])
-                return false;
-        return true;
+    int count [10] = {};
+    while (n > 0){
+        count [n % 10]++;
+        n /= 10;
     }
+    for (int i = 0; i < 10; i++)
+        if (freq [i] != count [i])
+            return false;
+    return true;
+}
 
 bool reorderedPowerOf2 (int n) {
     int freq [10] = {};
