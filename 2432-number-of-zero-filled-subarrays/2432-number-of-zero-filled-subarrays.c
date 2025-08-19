298ac888a795c262fb82753f0036eb8fe1b6b837
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-08-19 06:14:56 +0530

    Time: 0 ms (100%), Space: 17.6 MB (29.03%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.c
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.c
@@ -1,7 +1,7 @@
 long long zeroFilledSubarray (int* nums, int numsSize) {
     long long cnt = 0, streak = 0;
     for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == 0) {
+        if (nums [i] == 0) {
             streak++;
             cnt += streak;
         } else {
