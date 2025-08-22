1cff8e4e78cbcc8d09d4ed358f4dc74483c240a4
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-08-22 14:28:23 +0530

    Time: 434 ms (66.67%), Space: 55.8 MB (0%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.c
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.c
@@ -4,7 +4,7 @@ int minimumArea (int** grid, int gridSize, int* gridColSize) {
     int minCol = n, maxCol = -1;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (grid[i][j] == 1) {
+            if (grid [i] [j] == 1) {
                 if (i < minRow) minRow = i;
                 if (i > maxRow) maxRow = i;
                 if (j < minCol) minCol = j;
