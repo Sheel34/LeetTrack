0751881fe51296b3a624b3cd7dafea67809f98da
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-09-03 13:27:15 +0530

    Time: 46 ms (100%), Space: 45.5 MB (34.78%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
@@ -7,7 +7,7 @@ class Solution {
         int ans = 0;
         for (int i = 0; i < n - 1; i++) {
             int y = Integer.MAX_VALUE, yi = P [i] [1];
-            for(int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 final int yj = P [j] [1];
                 if (yj >= yi && y > yj) {
                     ans++;
