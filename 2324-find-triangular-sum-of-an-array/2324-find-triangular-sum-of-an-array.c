b352c720b0ff96063fca1dbdcc9088c570182b62
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-09-30 14:16:34 +0530

    Time: 51 ms (78.79%), Space: 8.9 MB (96.97%) - LeetHub

---
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.c
@@ -0,0 +1,7 @@
+int triangularSum (int* nums, int numsSize) {
+    int n = numsSize;
+    for (int size = n; size > 1; size--)
+        for (int i = 0; i < size - 1; i++)
+            nums [i] = (nums [i] + nums [i + 1]) % 10;
+    return nums [0];
+}
