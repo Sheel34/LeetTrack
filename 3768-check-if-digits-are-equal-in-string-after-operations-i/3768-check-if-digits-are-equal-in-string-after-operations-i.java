fe49575d34ebe269a728ac68daedfe5fede9fb1d
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-10-23 11:22:46 +0530

    Time: 1 ms (100%), Space: 42.3 MB (93.09%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean hasSameDigits (String s) {
-        int i = 0;
-        String res = "";
-        while (s.length () > 2 && i < s.length () - 1) {
-            res += (char) ((((s.charAt (i) - '0') + (s.charAt (i + 1) - '0')) % 10) + '0');
-            i++;
-            if (i == s.length () - 1) {
-                s = res;
-                i = 0;
-                res = "";
-            }
-        }
-        return s.length () == 2 && s.charAt (0) == s.charAt (1);
+        char [] a = s.toCharArray ();
+        for (int i = a.length - 1; i >= 2; i--)
+            for (int j = 0; j < i; j++)
+                a [j] = (char) ((a [j] + a [j + 1]) % 10 + '0');
+        return a [0] == a [1];
     }
 }
