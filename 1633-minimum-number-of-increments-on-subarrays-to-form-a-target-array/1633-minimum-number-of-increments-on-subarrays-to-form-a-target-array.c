44de82a6c1da164ffe975f43203345cc103f74ce
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-10-30 13:58:10 +0530

    Time: 6 ms (20%), Space: 15.6 MB (8%) - LeetHub

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.c
@@ -0,0 +1,8 @@
+#pragma GCC optimize ("O3, unroll-loops")
+
+int minNumberOperations (int* target, int targetSize) {
+    int ans = target [0];
+    for (int i = 1; i < targetSize; i++)
+        ans += fmax (target [i] - target [i - 1], 0);
+    return ans;
+}
