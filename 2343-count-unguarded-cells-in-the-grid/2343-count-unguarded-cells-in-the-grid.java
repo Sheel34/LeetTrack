4e0cea915820852b9cb9497a955cc993141d5a20
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-11-02 11:07:13 +0530

    Time: 371 ms (9.49%), Space: 221.1 MB (5.11%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,45 +1,39 @@
 class Solution {
-    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        
-        // Initialize grid with zeros
-        int[][] g = new int[m][n];
-        
-        // Mark guards and walls as 2
-        for (int[] e : guards) {
-            g[e[0]][e[1]] = 2;
+    public int countUnguarded (int m, int n, int [] [] guards, int [] [] walls) {
+        int [] [] vis = new int [m] [n];
+        Queue <int []> q = new LinkedList <> ();
+        Map <String, Integer> mp = new HashMap <> ();
+        for (int [] g : guards) {
+            q.add (g);
+            mp.put (g [0] + "," + g [1], 1);
+            vis [g [0]] [g [1]] = 1;
         }
-        for (int[] e : walls) {
-            g[e[0]][e[1]] = 2;
+        for (int [] w : walls) {
+            mp.put (w [0] + "," + w [1], 1);
+            vis [w [0]] [w [1]] = 1;
         }
-        
-        // Directions: up, right, down, left
-        int[] dirs = {-1, 0, 1, 0, -1};
-        
-        // Process each guard's line of sight
-        for (int[] e : guards) {
-            for (int k = 0; k < 4; ++k) {
-                int x = e[0], y = e[1];
-                int dx = dirs[k], dy = dirs[k + 1];
-                
-                // Check cells in current direction until hitting boundary or obstacle
-                while (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n && g[x + dx][y + dy] < 2) {
-                    x += dx;
-                    y += dy;
-                    g[x][y] = 1;
-                }
-            }
+        for (int [] g : guards) {
+            int r = g [0], c = g [1];
+            dfs (r, c + 1, "r", vis, mp);
+            dfs (r, c - 1, "l", vis, mp);
+            dfs (r + 1, c, "d", vis, mp);
+            dfs (r - 1, c, "u", vis, mp);
         }
-        
-        // Count unguarded cells (cells with value 0)
-        int unguardedCount = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (g[i][j] == 0) {
-                    unguardedCount++;
-                }
-            }
-        }
-        
-        return unguardedCount;
+        int cnt = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (vis [i] [j] == 0) cnt++;
+        return cnt;
+    }
+    void dfs (int r, int c, String dir, int [] [] vis, Map <String, Integer> mp) {
+        int n = vis.length;
+        int m = vis [0].length;
+        if (r < 0 || c < 0 || r >= n || c >= m) return;
+        if (mp.containsKey (r + "," + c)) return;
+        vis [r] [c] = 1;
+        if (dir.equals ("r")) dfs (r, c + 1, "r", vis, mp);
+        if (dir.equals ("l")) dfs (r, c - 1, "l", vis, mp);
+        if (dir.equals ("u")) dfs (r - 1, c, "u", vis, mp);
+        if (dir.equals ("d")) dfs (r + 1, c, "d", vis, mp);
     }
 }
