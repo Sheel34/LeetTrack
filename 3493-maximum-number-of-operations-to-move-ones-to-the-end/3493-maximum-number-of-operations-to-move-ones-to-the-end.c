14b69bdacda4a8b9fc8e1b2d6c510771b87d4e99
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-11-13 09:13:59 +0530

    Time: 1039 ms (0%), Space: 10.2 MB (66.67%) - LeetHub

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.c
@@ -0,0 +1,9 @@
+int maxOperations (char* s) {
+    int ones = 0, res = 0;
+    for (int i = 0; i < strlen (s); ++i)
+        if (s [i] == '1')
+            ones++;
+        else if ((i > 0) && s [i - 1] == '1')
+            res += ones;
+    return res;
+}
