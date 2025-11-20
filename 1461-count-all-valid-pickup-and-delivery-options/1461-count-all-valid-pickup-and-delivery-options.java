5dd75daeb9a79180e17bb71d830b4f4694b9ceed
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-11-20 20:14:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (18.71%) - LeetHub

---
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countOrders (int n) {
+        int mod = (int) 1e9 + 7;
+        long count = 1;
+        for (int i = 2; i <= n; i++)
+            count = (count * (2 * i - 1) * i) % mod;
+        return (int) count % mod;
+    }
+}
