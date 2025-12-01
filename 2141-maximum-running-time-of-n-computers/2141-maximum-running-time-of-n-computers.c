eed46770f8713d359b17c71ade3d01f4fcb94dbf
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-12-01 12:18:38 +0530

    Time: 43 ms (100%), Space: 17.8 MB (100%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.c
@@ -0,0 +1,16 @@
+int cmp (const void* a, const void* b) {
+    return (*(int*) a) - (*(int*) b);
+}
+
+long long maxRunTime (int n, int* batteries, int batteriesSize) {
+    long long totalEnergy = 0;
+    for (int i = 0; i < batteriesSize; i++) totalEnergy += batteries [i];
+    qsort (batteries, batteriesSize, sizeof (int), cmp);
+    for (int i = batteriesSize - 1; i >= 0; i--) {
+        if (batteries [i] > totalEnergy / n) {
+            totalEnergy -= batteries [i];
+            n--;
+        } else break;
+    }
+    return totalEnergy / n;
+}
