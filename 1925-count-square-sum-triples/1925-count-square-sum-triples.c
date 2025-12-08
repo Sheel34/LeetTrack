8d70f2302cc63c36992e6d6d9d9c38a0ef0635f5
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-12-08 15:31:47 +0530

    Time: 0 ms (100%), Space: 7.8 MB (25%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.c
@@ -0,0 +1,31 @@
+#pragma GCC optimize ("O3, unroll-loops")
+
+int GCD [23] [23] = {{0}};
+
+void GCD_ () {
+    if (GCD [1] [1]) return;
+    const int N = 23;
+    for(int i = 1; i < N; i++) {
+        GCD [i] [0] = GCD [0] [i] = GCD [i] [i] = i;
+        for(int j = i + 1; j < N; j++) {
+            int r = j - i;
+            GCD [i] [j] = GCD [j] [i] = GCD [i] [r];
+        }
+    }
+}
+
+int countTriples (int n) {
+    GCD_ ();
+    int cnt = 0;
+    int nsqrt = sqrt (n);
+    for (int s = 2; s <= nsqrt; s++) {
+        for (int t = 1 + (s & 1); t < s; t += 2) {
+            if (GCD [s] [t] != 1) continue;
+            int c = s * s + t * t;
+            if (c > n) break;
+            int kmax = n / c;
+             cnt += 2 * kmax;
+        }
+    }
+    return cnt;
+}
