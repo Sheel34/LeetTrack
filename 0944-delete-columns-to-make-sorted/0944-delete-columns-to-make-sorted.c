6b39a2525be50968ce56316f64e12a29c0c59a48
Sheel Patel <144551565+Sheel34@users.noreply.github.com>
2025-12-21 00:09:45 +0530

    Time: 0 ms (100%), Space: 10.4 MB (72.22%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.c
@@ -0,0 +1,12 @@
+#pragma GCC optimize ("O3, unroll-loops")
+
+int minDeletionSize (char** strs, int strsSize) {
+    int cnt = 0;
+    for (int j = 0; strs [0] [j] != '\0'; j++)
+        for (int i = 0; i < strsSize - 1; i++)
+            if (strs [i] [j] > strs [i + 1] [j]) {
+                cnt++;
+                break;
+            }
+    return cnt;
+}
